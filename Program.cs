4c036e89aa0e65b73a677d910cb18fa65ab58ca4
islomdoda <bekjonxonkim@gmail.com>
2024-05-16 17:45:01 +0500

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+System.Console.WriteLine("Type 1st number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Type 2nd number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"1st number is greater than 2nd number: {firstNumber > secondNumber}");
+System.Console.WriteLine($"1st number is greater than or equal to 2nd number: {firstNumber >= secondNumber}");
+System.Console.WriteLine($"1st number is less than 2nd number: {firstNumber < secondNumber}");
+System.Console.WriteLine($"1st number is less than or equal to 2nd number: {firstNumber <= secondNumber}");
+System.Console.WriteLine($"Number are equal: {firstNumber == secondNumber}");
+System.Console.WriteLine($"Number are not equal: {firstNumber != secondNumber}");
